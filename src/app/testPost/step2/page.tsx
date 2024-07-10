d739a49f3a474c37f7bea08c660891b04af07228
sakaaya <space.star.1016.f@gmail.com>
2024-07-10 23:40:54 +0900

    put on gtm all page

--- src/app/testPost/step2/page.tsx
+++ src/app/testPost/step2/page.tsx
@@ -1,27 +1,6 @@
-'use client';
 import Link from 'next/link';
-import { useEffect } from 'react';
 
 export default function App() {
-  useEffect(() => {
-    let script = null;
-    if (document.querySelector('#GTM-test') === null) {
-      script = document.createElement('script');
-      script.id = 'GTM-test';
-      script.innerHTML = `
-            <!-- Google tag (gtag.js) -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-W7P01QQX97"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-      
-              gtag('config', 'G-W7P01QQX97');
-            </script>
-              `;
-      document.head.appendChild(script);
-    }
-  }, []);
   return (
     <>
       <h1>Thanks Page</h1>
